f96575b1db89b3826201094190cd9b68aeec6e2c
aburossi <pietro.rossi@bbw.ch>
2023-11-04 21:09:19 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "aburossi.github.io",
+    baseUrl: "aburossi.github.io/quartz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
