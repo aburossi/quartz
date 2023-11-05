4079c9c4a4fd0a34117af33b83d9083acb545519
aburossi <pietro.rossi@bbw.ch>
2023-11-05 18:11:28 +0100

    Update Head.tsx

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -17,7 +17,7 @@ export default (() => {
 
     return (
       <head>
-        <title>ABU</title>
+        <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
