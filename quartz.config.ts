914e8196c004329ebdcaa763c98eb95602436ad8
aburossi <142565006+aburossi@users.noreply.github.com>
2023-11-16 22:36:41 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Rechtskunde",
+    pageTitle: "ABU",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
